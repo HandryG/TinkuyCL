a9917e8e632a98d79af7dae196bc351cc00d2e52
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 02:07:31 -0500

    Update the_clustering.py

--- the_clustering.py
+++ the_clustering.py
@@ -1,8 +1,9 @@
 from sklearn.cluster import OPTICS
-from data_io import get_tinkuy_coords_df 
+from data_io import get_tinkuy_coordsdf 
 
 def do_clustering():
   df = get_tinkuy_coords_df()
+  df = pd.to_numeric(df[["latitud","longitud"]], downcast="float")
   df = df[["latitud","longitud"]].to_numpy()
   clust  = OPTICS(min_samples=15).fit_predict(df)
   return clust
