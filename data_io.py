38b1428d84030a26b9d1c3726bac673b1b08317d
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 01:27:57 -0500

    Env variables for access keys

--- data_io.py
+++ data_io.py
@@ -1,11 +1,12 @@
 import boto3
 import pandas as pd
+import os
 
 def get_tinkuy_coords_df():
     dynamodb = boto3.client('dynamodb',\
-                      aws_access_key_id='AKIAIN7WLEJ3I6L4JQSQ',\
-                      aws_secret_access_key='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',\
-                      region_name='us-east-1')
+                      aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID'],\
+                      aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY'],\
+                      region_name = os.environ['AWS_DEFAULT_REGION'])
     response = dynamodb.scan(TableName='tinkuy-coords')
     df = pd.DataFrame.from_dict(response['Items'])
     return df
