14cee7d1833cddb84bac25c15007fda7c837862b
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 05:07:08 -0500

    Update data_io.py

--- data_io.py
+++ data_io.py
@@ -29,3 +29,21 @@ def get_tinkuy_coords_np():
         points.append(points)
         
     return np.array(points)
+
+def get_tinkuy_coords_list():
+    dynamodb = boto3.client('dynamodb',\
+                      aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID'],\
+                      aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY'],\
+                      region_name = os.environ['AWS_DEFAULT_REGION'])
+    response = dynamodb.scan(TableName='tinkuy-coords')
+    
+    points = []
+    i = 1
+    for item in response['Items']:
+        #print(item)
+        point = [float(item['latitud']['S']),float(item['longitud']['S'])]
+        print(point)
+        i += 1
+        points.append(points)
+        
+    return points
