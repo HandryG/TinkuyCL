9e2ee39157fe1fa730ae2888ac9bd77ef2761a06
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 05:46:50 -0500

    Update the_clustering.py

--- the_clustering.py
+++ the_clustering.py
@@ -6,9 +6,11 @@ from pyclustering.cluster import cluster_visualizer
 from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from pyclustering.cluster.kmedoids import kmedoids
 from pyclustering.cluster.silhouette import silhouette_ksearch_type, silhouette_ksearch
+import sys
 
 def do_clustering(min_medoids,max_medoids):
   points = get_tinkuy_coords_list()
+  sys.setrecursionlimit(2500)
   search_instance = silhouette_ksearch(points, 2, 10, algorithm=silhouette_ksearch_type.KMEANS).process()
   amount = search_instance.get_amount()
   scores = search_instance.get_scores()
