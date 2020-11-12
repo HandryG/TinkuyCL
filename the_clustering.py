76fb2725fb60eb634650be882909dbf78b51b4bc
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 05:30:21 -0500

    Update the_clustering.py

--- the_clustering.py
+++ the_clustering.py
@@ -1,10 +1,29 @@
-from sklearn.cluster import OPTICS
-from data_io import get_tinkuy_coords_np 
-import pandas as pd
+#from sklearn.cluster import OPTICS
+#import pandas as pd
 
-def do_clustering():
-  df = get_tinkuy_coords_np()
-  clust  = OPTICS(min_samples=15).fit_predict(df)
-  print('Cluster model done')
-  return clust
+from data_io import get_tinkuy_coords_np, get_tinkuy_coords_list
+from pyclustering.cluster import cluster_visualizer
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+from pyclustering.cluster.kmedoids import kmedoids
+from pyclustering.cluster.silhouette import silhouette_ksearch_type, silhouette_ksearch
+
+def do_clustering(min_medoids,max_medoids):
+  points = get_tinkuy_coords_list()
+  search_instance = silhouette_ksearch(points, min_medoids, max_medoids, algorithm=silhouette_ksearch_type.KMEDOIDS).process()
+  amount = search_instance.get_amount()
+  scores = search_instance.get_scores()
+  k = max(scores, key=scores.get)
+  initial_centers = kmeans_plusplus_initializer(points, amount).initialize()
+  kmeans_instance = kmedoids(points, range(0,amount)).process()
+  clusters   = kmeans_instance.get_clusters()
+  medoids    = kmeans_instance.get_medoids()
+  visualizer = cluster_visualizer()
+  visualizer.append_clusters(clusters, points)
+  #visualizer.show()
+
+  medoid_points = []
+  for medoid in medoids:
+      medoid_points.append(points[medoid])
+  
+  return medoid_points
 
