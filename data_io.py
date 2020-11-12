cddd807281c55119fd2591d7cc8bc515fb8041bd
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 02:42:06 -0500

    Update data_io.py

--- data_io.py
+++ data_io.py
@@ -1,6 +1,7 @@
 import boto3
 import pandas as pd
 import os
+import numpy as np
 
 def get_tinkuy_coords_df():
     dynamodb = boto3.client('dynamodb',\
@@ -10,3 +11,17 @@ def get_tinkuy_coords_df():
     response = dynamodb.scan(TableName='tinkuy-coords')
     df = pd.DataFrame.from_dict(response['Items'])
     return df
+
+def get_tinkuy_coords_np():
+    dynamodb = boto3.client('dynamodb',\
+                      aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID'],\
+                      aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY'],\
+                      region_name = os.environ['AWS_DEFAULT_REGION'])
+    response = dynamodb.scan(TableName='tinkuy-coords')
+    
+    points = np.empty((0,2), float)
+    for item in response['Items']:
+        point = [float(item['latitud']['S']),float(item['longitud']['S'])]
+        np.append(points,np.array([point]), axis=0,)
+        
+    return points
