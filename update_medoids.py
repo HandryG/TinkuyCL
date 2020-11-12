671b3ec50da7278ad0443929cfc8f12c547ff245
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 07:09:26 -0500

    Create update_medoids.py

---
+++ update_medoids.py
@@ -0,0 +1,22 @@
+import boto3
+import os
+import time
+
+medoid_list = {}
+medoid_list['list']=[]
+
+point_list = do_clustering(3,10)
+medoid_list['tstamp'] = time.time() 
+medoid_list['points'] = point_list
+medoid_list['id'] = 1
+print(medoid_list)
+
+dynamodb = boto3.resource('dynamodb',\
+                      aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID'],\
+                      aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY'],\
+                      region_name = os.environ['AWS_DEFAULT_REGION'])
+                  
+table = dynamodb.Table('tinkuy-clusters')
+table.delete_item(Key={'id': id})
+table.put_item(medoid_list)
+  
