edfb37dc18aaa67580b4f7f3db9bc6095e8aae7d
moshe-exe <43896453+moshe-exe@users.noreply.github.com>
2020-11-12 02:42:37 -0500

    Update the_clustering.py

--- the_clustering.py
+++ the_clustering.py
@@ -3,10 +3,10 @@ from data_io import get_tinkuy_coords_df
 import pandas as pd
 
 def do_clustering():
-  df = get_tinkuy_coords_df()
-  df = pd.to_numeric(df["latitud"], downcast="float")
-  df = pd.to_numeric(df["longitud"], downcast="float")
-  df = df[["latitud","longitud"]].to_numpy()
+  df = get_tinkuy_coords_np()
+  #df = pd.to_numeric(df["latitud"], downcast="float")
+  #df = pd.to_numeric(df["longitud"], downcast="float")
+  #df = df[["latitud","longitud"]].to_numpy()
   clust  = OPTICS(min_samples=15).fit_predict(df)
   return clust
 
